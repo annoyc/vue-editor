352b80cb7eb71478ce0b493ee383b9d0d91670db
kevinawe <yc2hangycz@gmail.com>
2022-05-14 14:28:59 +0800

    chore: construct monorepo structure

--- apps/editor/shims.d.ts
+++ apps/editor/shims.d.ts
@@ -4,7 +4,7 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
-
+// declare module '@zyc/shared';
 declare module '@vue/runtime-dom' {
   interface HTMLAttributes extends AttributifyAttributes {}
 }
